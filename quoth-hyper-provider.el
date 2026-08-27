e649201c20a3ca24e4e4e5c14edbad513d90312f
Thomas Christensen <thomasc1971@hotmail.com>
2026-08-27 19:45:14 +0700

    Remove Version headers from library/test files
    Only quoth.el carries the canonical Version: header per Emacs
    packaging convention. The other files were out of sync and
    would require needless updates on every release.

--- quoth-hyper-provider.el
+++ quoth-hyper-provider.el
@@ -3,7 +3,6 @@
 
 ;;; Author: Thomas Christensen <thomasc1971@hotmail.com>
 ;;; URL: https://github.com/thomasc1971/quoth
-;;; Version: 0.1.0
 ;;; Package-Requires: ((emacs "28.1"))
 ;;; Keywords: tools, ai, convenience
 ;;; Prefix: quoth-
