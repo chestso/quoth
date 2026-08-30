81ddc321ac4cfb37ff405673634ff67d41a7367f
Thomas Christensen <thomasc1971@hotmail.com>
2026-08-30 16:57:40 +0700

    Release v0.3.0

--- quoth.el
+++ quoth.el
@@ -3,7 +3,7 @@
 
 ;;; Author: Thomas Christensen <thomasc1971@hotmail.com>
 ;;; URL: https://github.com/chestso/quoth
-;;; Version: 0.2.1
+;;; Version: 0.3.0
 ;;; Package-Requires: ((emacs "28.1"))
 ;;; Keywords: tools, ai, convenience
 ;;; Prefix: quoth-
