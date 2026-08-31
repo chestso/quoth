51e238b1a8e8a76bafdae9a41646ff55cdfcd339
Thomas Christensen <thomasc1971@hotmail.com>
2026-08-31 11:38:01 +0700

    Bump version references to v0.3.1

--- quoth.el
+++ quoth.el
@@ -3,7 +3,7 @@
 
 ;;; Author: Thomas Christensen <thomasc1971@hotmail.com>
 ;;; URL: https://github.com/chestso/quoth
-;;; Version: 0.3.0
+;;; Version: 0.3.1
 ;;; Package-Requires: ((emacs "28.1"))
 ;;; Keywords: tools, ai, convenience
 ;;; Prefix: quoth-
