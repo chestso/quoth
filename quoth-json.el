382c73181a5646094b62814c9d5cd27e4765513c
Thomas Christensen <thomasc1971@hotmail.com>
2026-09-01 13:22:17 +0700

    Align quoth-json file headers with the project convention
    quoth-json.el and test/quoth-test-json.el carried a non-standard header:
    2-semicolon comment lines, no URL/Package-Requires/Keywords/Prefix lines,
    and a GPL notice instead of the project's MIT notice.  Bring both in line
    with every other .el file: 3-semicolon header block (Author, URL,
    Package-Requires, Keywords, Prefix), the "not part of GNU Emacs" note,
    and the MIT license block.  Move the test file's declare-function stubs
    into the Code section, the conventional location.

--- quoth-json.el
+++ quoth-json.el
@@ -1,16 +1,31 @@
-;;; quoth-json.el --- JSON decode/encode abstraction, native C when possible  -*- lexical-binding: t; -*-
+;;; quoth-json.el --- JSON decode/encode abstraction for quoth  -*- lexical-binding: t; -*-
+;;; Copyright (C) 2026 Thomas Christensen
 
-;; Copyright (C) 2026  Thomas Christensen
+;;; Author: Thomas Christensen <thomasc1971@hotmail.com>
+;;; URL: https://github.com/chestso/quoth
+;;; Package-Requires: ((emacs "28.1"))
+;;; Keywords: tools, ai, convenience
+;;; Prefix: quoth-
 
-;; Author: Thomas Christensen <thomasc1971@hotmail.com>
-;; Keywords: comm, tools
+;;; This file is not part of GNU Emacs.
 
-;; This file is part of Quoth.
+;;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;;; of this software and associated documentation files (the "Software"), to deal
+;;; in the Software without restriction, including without limitation the rights
+;;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;; copies of the Software, and to permit persons to whom the Software is
+;;; furnished to do so, subject to the following conditions:
 
-;; Quoth is free software; you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;;; The above copyright notice and this permission notice shall be included in all
+;;; copies or substantial portions of the Software.
+
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+;;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;;; SOFTWARE.
 
 ;;; Commentary:
 
--- test/quoth-test-json.el
+++ test/quoth-test-json.el
@@ -1,7 +1,30 @@
-;;; quoth-test-json.el --- tests for the quoth-json abstraction  -*- lexical-binding: t; -*-
+;;; quoth-test-json.el --- JSON abstraction tests for quoth  -*- lexical-binding: t; -*-
+;;; Copyright (C) 2026 Thomas Christensen
 
-(declare-function quoth-json-read "quoth-json" (json-string))
-(declare-function quoth-json-write "quoth-json" (object))
+;;; Author: Thomas Christensen <thomasc1971@hotmail.com>
+;;; URL: https://github.com/chestso/quoth
+;;; Package-Requires: ((emacs "28.1"))
+;;; Keywords: tools, ai, convenience
+
+;;; This file is not part of GNU Emacs.
+
+;;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;;; of this software and associated documentation files (the "Software"), to deal
+;;; in the Software without restriction, including without limitation the rights
+;;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;; copies of the Software, and to permit persons to whom the Software is
+;;; furnished to do so, subject to the following conditions:
+
+;;; The above copyright notice and this permission notice shall be included in all
+;;; copies or substantial portions of the Software.
+
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+;;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;;; SOFTWARE.
 
 ;;; Commentary:
 ;;; Exercises `quoth-json-read' / `quoth-json-write': alist shape,
@@ -9,6 +32,9 @@
 ;;; malformed-input behavior.
 
 ;;; Code:
+
+(declare-function quoth-json-read "quoth-json" (json-string))
+(declare-function quoth-json-write "quoth-json" (object))
 
 (require 'ert)
 (require 'cl-lib)
